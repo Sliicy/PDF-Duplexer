4d6cddd794b47777ec1be7256d857466faec75b6
Sliicy <Sliicy@users.noreply.github.com>
2020-04-30 17:29:36 -0400

    Initial Commit

---
+++ PDF Duplexer/Main Form.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace PDF_Duplexer {
+    partial class FrmMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnPrint.Location = new System.Drawing.Point(15, 15);
+            this.btnPrint.Margin = new System.Windows.Forms.Padding(6);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(306, 51);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "&Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.BtnPrint_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(15, 78);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(6);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(306, 51);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 144);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPrint);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF Duplexer";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ PDF Duplexer/Main Form.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iText.Kernel.Pdf;
+
+namespace PDF_Duplexer {
+    public partial class FrmMain : Form {
+        public FrmMain() {
+            InitializeComponent();
+        }
+
+        public void SendToPrinter(string filePath, int pageCount) {
+            Cursor.Current = Cursors.WaitCursor;
+            ProcessStartInfo info = new ProcessStartInfo() {
+                Verb = "print",
+                FileName = filePath,
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            Process p = new Process() {
+                StartInfo = info
+            };
+            p.Start();
+            p.WaitForInputIdle();
+
+            // Wait minimum 3 seconds plus however many pages are being printed:
+            if (pageCount < 3) {
+                pageCount = 3;
+            }
+            System.Threading.Thread.Sleep(1000 * pageCount);
+
+            if (false == p.CloseMainWindow())
+                p.Kill();
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void CleanUp(string path1, string path2) {
+            File.Delete(path1);
+            File.Delete(path2);
+        }
+        private void Print() {
+            // Protects accidentally deleting an existing file in Downloads:
+            Random random = new Random();
+            int randomNumber = random.Next(0, 10000);
+
+            string destinationOdd = @"C:\Users\" + Environment.UserName + @"\Downloads\odd temp" + randomNumber.ToString() + ".pdf";
+            string destinationEven = @"C:\Users\" + Environment.UserName + @"\Downloads\even temp" + randomNumber.ToString() + ".pdf";
+
+            var dir = new DirectoryInfo(@"C:\Users\" + Environment.UserName + @"\Downloads");
+            var pdfFile = (from f in dir.GetFiles()
+                           orderby f.LastWriteTime
+                           select f).Last();
+            if (pdfFile.Extension.ToLower() != ".pdf") {
+                MessageBox.Show("Please download a PDF first to print it double sided!");
+                return;
+            }
+            DialogResult dialog1 = MessageBox.Show("Would you like to print " + pdfFile.Name + " double sided?", "Print " + pdfFile.Name + " double sided?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialog1 == DialogResult.Yes) {
+                PdfDocument doc = new PdfDocument(new PdfReader(pdfFile.FullName));
+
+                int numberOfPages = doc.GetNumberOfPages();
+
+                List<int> odds = new List<int>();
+                List<int> evens = new List<int>();
+                for (var i = 1; i <= numberOfPages; i++) {
+                    if (i % 2 != 0)
+                        odds.Add(i);
+                }
+                for (int i = 1; i <= numberOfPages; i++) {
+                    if (i % 2 == 0)
+                        evens.Add(i);
+                }
+                evens.Reverse();
+
+                PdfDocument oddPDFWriter = new PdfDocument(new PdfWriter(destinationOdd));
+                PdfDocument evenPDFWriter = new PdfDocument(new PdfWriter(destinationEven));
+
+                 //If odd number of pages, append blank page to beginning of even pages:
+                if (numberOfPages % 2 != 0) {
+                    evenPDFWriter.AddNewPage();
+                }
+
+                foreach (var oddPage in odds)
+                    doc.CopyPagesTo(oddPage, oddPage, oddPDFWriter);
+                foreach (var evenPage in evens)
+                    doc.CopyPagesTo(evenPage, evenPage, evenPDFWriter);
+
+                int oddPageCount = oddPDFWriter.GetNumberOfPages();
+                int evenPageCount = evenPDFWriter.GetNumberOfPages();
+
+                oddPDFWriter.Close();
+                evenPDFWriter.Close();
+
+                // Print odd side:
+                SendToPrinter(destinationOdd, oddPageCount);
+
+                DialogResult dialog2 = MessageBox.Show("Please put the papers back into the printer face down. Then press OK to continue.", "Put back into printer", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if (dialog2 != DialogResult.OK) {
+                    CleanUp(destinationOdd, destinationEven);
+                    return;
+                }
+
+                // Print even side:
+                SendToPrinter(destinationEven, evenPageCount);
+
+                // Clean up:
+                CleanUp(destinationOdd, destinationEven);
+                return;
+            } else
+                return;
+        }
+        private void FrmMain_Load(object sender, EventArgs e) {
+            
+        }
+
+        private void BtnPrint_Click(object sender, EventArgs e) {
+            Print();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+    }
+}
